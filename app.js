c26eada678fe432480ee02a937f8a43e3de41c60
Tom Kadwill <tomkadwill@gmail.com>
2019-12-11 08:17:36 +0000

    Revert "Revert "allow port to be specified by Heroku""
    This reverts commit a20574a5f5c50eee5e75fed70cbf18ed3365d078.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const urlShortener = require('node-url-shortener');
 
 const bodyParser = require('body-parser')
