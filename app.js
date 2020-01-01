d5972510abcdd22fd834ca4d1c0775cd6f59ce15
Tom Kadwill <tomkadwill@gmail.com>
2020-01-01 11:25:40 +0000

    Add logic for persisting urls

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 const urlShortener = require('node-url-shortener');
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
+const db = require('./models/index.js');
 
-const bodyParser = require('body-parser')
+const sequelize = new Sequelize('urlshortener', 'tomkadwill', 'password', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
@@ -15,8 +22,11 @@ app.get('/', function(req, res) {
 app.post('/url', function(req, res) {
   const url = req.body.url
 
-  urlShortener.short(url, function(err, shortUrl){
-    res.send(shortUrl);
+  urlShortener.short(url, function(err, shortUrl) {
+    db.Url.findOrCreate({where: {url: url, shortUrl: shortUrl}})
+          .then(([urlObj, created]) => {
+            res.send(shortUrl)
+          });
   });
 });
 
